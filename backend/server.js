8c66229f491bddcf6d9d8e05d55827a8b7981d35
Teshie <teshomeaychiluhem@gmail.com>
2022-06-28 05:36:47 -0700

    database connection setup successfully

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 require(`dotenv`).config();
+const connectDB = require("./config/db");
 const express = require("express");
 const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
